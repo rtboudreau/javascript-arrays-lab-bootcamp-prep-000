812e22ce07f541dbddda37fd0a2b61d1d2579bbf
Reid Boudreau <rtboudreau@gmail.com>
2018-12-16 23:02:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 var cassie = "Cassie";
 // Add your functions and code here
 
-function destructivelyAppendKitten(kittens, cassie){
+function destructivelyAppendKitten(cassie){
   kittens.push(cassie);
   return kittens;
 }
